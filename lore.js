1e86323095188fe1c7cd7182ec82e1ff44841f0a
RhysticGamesNZ <carr.jordan93@gmail.com>
2025-04-24 12:00:30 +1200

    Update lore.js

--- lore.js
+++ lore.js
@@ -50,5 +50,9 @@ async function loadLore() {
     container.appendChild(categoryDetails);
   }
 }
+document.getElementById("hamburger-icon").addEventListener("click", function() {
+  const navLinks = document.querySelector(".nav-links");
+  navLinks.classList.toggle("active"); // Toggle visibility of navigation links
+});
 
 loadLore();
