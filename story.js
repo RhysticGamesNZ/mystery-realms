e8e7b3dc76cf3b4368df44e4876ece89e47754e9
RhysticGamesNZ <carr.jordan93@gmail.com>
2025-04-30 20:29:57 +1200

    Update story.js

--- story.js
+++ story.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-const db = getFirestore();
+const db = getFirestore(app);
 const container = document.getElementById("chapter-container");
 
 async function loadPremiumStory() {
